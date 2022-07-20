21441aaef9b1c3a53635fe5ecf06761f997e4d08
eronmiranda <mirandaeronielle@gmail.com>
2022-07-19 18:35:35 -0600

    Recreate solution directory / boilerplate
    to just use react without typescript. Will add typescript afterwards

---
+++ stock-ticker-app/src/App.js
@@ -0,0 +1,16 @@
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import * as React from 'react';
+function App() {
+  return (
+    <Container maxwidth="sm">
+      <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }}>
+        <div>Welcome to Stock Ticker App</div>
+      </Box>
+      
+    </Container>
+
+  );
+}
+
+export default App;
